b8c0c46b0854ad681f5d9f689a8331e3c2957d18
Sanim1195 <57519459+Sanim1195@users.noreply.github.com>
2021-12-09 21:33:16 +1300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        //$this->call(OrdersSeeder::class);
-        //$this->call(FoodsSeeder::class);
+        $this->call(UserTypesSeeder::class);
+         $this->call(UsersSeeder::class);
+        $this->call(OrdersSeeder::class);
+          $this->call(OrderItemsSeeder::class);        
+        $this->call(FoodsSeeder::class);
        $this->call(OrderItemsSeeder::class);
     }
 }
